8b5b2ed71a523a085b43dcc5ad962d39f97b501e
HeruSubakt07 <114379114+HeruSubakt07@users.noreply.github.com>
2022-09-26 12:28:43 +0700

    Add files via upload

---
+++ Tugasno2PBO.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+public class Tugasno2PBO {
+     public static void main(String[] args) {
+        String Pesan;
+         Pesan = " ";
+         Pesan = JOptionPane.showInputDialog("Anda Sedang Belajar Apa ?");
+        String pesan = ( "belajar " + Pesan + " sangat mudah");
+        JOptionPane.showMessageDialog(null, pesan);        
+    }
+}
+
+/*
+Nama : Heru Subakti
+Kelas :TI21A
+Tugas Sesi 1 No 2
+ */
